4fdf91b5d87e92f79799c904f2ff8830ce532f21
Tommi Alajoki <tommi@alajoki.fi>
2021-12-19 01:06:30 +0200

    Enabled browsing of months, fixed db writing

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import datetime #datetime lib
-import calendar
-from tkinter.constants import BOTTOM, LEFT, TOP #calendar object for easier printing so we don't have to just reinvent it with datetime arrays
+import calendar #calendar object for easier printing so we don't have to just reinvent it with datetime arrays
 from colorama import Style, Fore, init #For coloured output in bash
 init(convert=True) #Coloured text doesn't work with git bash out the box so added this to help
-import tkinter
-from tkcalendar import *
-import csv
+import tkinter # For GUI
+from tkcalendar import * #Self made helper class for visualizing the calendar
+import csv # To read/write to the "db" file
 
 #helper function to add to our task array for both reading DB and parsing a new task
 def addtask(date, task):
+    global tasks
     if date not in tasks.keys(): #If the date is not yet in the dict add it
         tasks[date] = [task]
     else:
@@ -31,28 +31,73 @@ def parsetask():
         addtask(date, task)
     except ValueError:
         pass
-    print(tasks)
 
 app = tkinter.Tk()
 app.title('Calendar')
-app.geometry('450x300')
+app.geometry('200x250')
+
+current_date = datetime.date.today()
+current = tkinter.StringVar()
+current.set(current_date.strftime("%B %Y"))
+
+def prevmonth():
+    global cal_frame
+    #Update the title
+    lastMonth = datetime.datetime.strptime(current.get(), "%B %Y") + datetime.timedelta(days=-1)
+    current.set(lastMonth.strftime("%B %Y"))
+    newFrame = tkcalendar.formatmonth(app, lastMonth.year, lastMonth.month)
+    if cal_frame is not None:
+        cal_frame.destroy()
+    cal_frame = newFrame
+    newFrame.pack()
+
+
+def nextmonth():
+    global cal_frame
+    #Update the title
+    nextMonth = datetime.datetime.strptime(current.get(), "%B %Y") + datetime.timedelta(weeks=4, days=3)
+    current.set(nextMonth.strftime("%B %Y"))
+    newFrame = tkcalendar.formatmonth(app, nextMonth.year, nextMonth.month)
+    if cal_frame is not None:
+        cal_frame.destroy()
+    cal_frame = newFrame
+    newFrame.pack()
 
 tkcalendar = TkCalendar(list(tasks.keys()))
 
-tkinter.Label(app, text='{0:%B} {0:%Y}'.format(datetime.date.today())).pack()
+header = tkinter.Frame(app)
+header.pack()
 
-frame = tkcalendar.formatmonth(app, 2021, 12)
-frame.pack()
+tkinter.Button(header, text='<-', command=prevmonth).grid(column=0, row=0, sticky='w')
+monthLabel = tkinter.Label(header, textvariable=current, font="Roboto 14 bold")
+monthLabel.grid(column = 1, row = 0)
+tkinter.Button(header, text='->', command=nextmonth).grid(column=2, row=0, sticky='e')
+
+cal_frame = tkcalendar.formatmonth(app, current_date.year, current_date.month)
+cal_frame.pack()
+
+TaskWindow = tkinter.Tk()
+TaskWindow.title('Tasks')
+
+tasks_frame = tkinter.Frame(TaskWindow)
+tasks_frame.pack()
+
+tkinter.Label(tasks_frame, text='Today\'s tasks', font='Roboto 12 bold').pack()
+for date, entries in tasks.items():
+    if date.day == datetime.date.today().day:
+        for task in entries:
+            task_label = tkinter.Label(tasks_frame, text=task)
+            task_label.pack()
 
 entry_frame = tkinter.Frame(app)
-entry_frame.pack()
+entry_frame.pack(side=tkinter.BOTTOM)
 
 tkinter.Label(entry_frame, text="Enter date:").grid(column = 0, row= 0)
-date_entry = tkinter.Entry(entry_frame, width=20)
+date_entry = tkinter.Entry(entry_frame, width=22)
 date_entry.grid(column = 1, row= 0, sticky='w')
 
 tkinter.Label(entry_frame, text="Enter task:").grid(column = 0, row= 1)
-task_entry = tkinter.Entry(entry_frame, width=40)
+task_entry = tkinter.Entry(entry_frame, width=22)
 task_entry.grid(column = 1, row= 1)
 
 btn = tkinter.Button(entry_frame, text = 'Add task', command=parsetask)
@@ -60,9 +105,11 @@ btn.grid(column = 0, row= 2)
 
 #Launch the GUI
 app.mainloop()
+TaskWindow.mainloop()
 
 #After GUI closes write the tasks back to file (overwriting it)
-#Figure this is better practice than opening the file whenever we want to write something into it
+#Probably a bit less resource intensive than writing to file every time a change happens
+#Also enables deletion
 with open('tasks.csv', 'w', newline='') as file:
     writer = csv.writer(file, delimiter=';')
     for date, tasks in tasks.items():
