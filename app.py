e0c3310414de43a3377c8f73947c806724db111c
Tommi Alajoki <tommi@alajoki.fi>
2021-12-19 15:17:50 +0200

    Added alert for wrong format of date and catch for nonexisting db file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import calendar #calendar object for easier printing so we don't have to just re
 from colorama import Style, Fore, init #For coloured output in bash
 init(convert=True) #Coloured text doesn't work with git bash out the box so added this to help
 import tkinter # For GUI
+from tkinter import messagebox
 from tkcalendar import * #Self made helper class for visualizing the calendar
 import csv # To read/write to the "db" file
 
@@ -16,11 +17,14 @@ def addtask(date, task):
 
 #Load tasks from file into a dict on initial load
 tasks = {}
-with open('tasks.csv', newline='') as file:
-    reader = csv.reader(file, delimiter=';')
-    for row in reader:
-        date = datetime.datetime.fromisoformat(row[0])
-        addtask(date, row[1])
+try:
+    with open('tasks.csv', 'r',newline='') as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            date = datetime.datetime.fromisoformat(row[0])
+            addtask(date, row[1])
+except FileNotFoundError: #Catch if the file doesn't exist
+    open('tasks.csv', 'w+')
 
 #Parse new task on button press and add it to the dict if it was valid
 def parsetask():
@@ -29,60 +33,58 @@ def parsetask():
     try:
         date = datetime.datetime.strptime(date, "%d.%m.%Y")
         addtask(date, task)
+        #Clear fields
+        date_entry.delete(0, 'end')
+        task_entry.delete(0, 'end')
     except ValueError:
-        pass
+        messagebox.showerror(title='Wrong format', message='Please use dd.mm.yyyy format for date.')
 
 app = tkinter.Tk()
 app.title('Calendar')
-app.geometry('200x250')
 
 current_date = datetime.date.today()
 current = tkinter.StringVar()
 current.set(current_date.strftime("%B %Y"))
 
-def prevmonth():
+def changemonth(date):
     global cal_frame
-    #Update the title
-    lastMonth = datetime.datetime.strptime(current.get(), "%B %Y") + datetime.timedelta(days=-1)
-    current.set(lastMonth.strftime("%B %Y"))
-    newFrame = tkcalendar.formatmonth(app, lastMonth.year, lastMonth.month)
+    current.set(date.strftime("%B %Y"))
+    newFrame = tkcalendar.formatmonth(app, date.year, date.month)
     if cal_frame is not None:
         cal_frame.destroy()
     cal_frame = newFrame
     newFrame.pack()
 
+def prevmonth():
+    global cal_frame
+    #Update the title
+    lastMonth = datetime.datetime.strptime(current.get(), "%B %Y") + datetime.timedelta(days=-1)
+    changemonth(lastMonth)
 
 def nextmonth():
     global cal_frame
     #Update the title
     nextMonth = datetime.datetime.strptime(current.get(), "%B %Y") + datetime.timedelta(weeks=4, days=3)
-    current.set(nextMonth.strftime("%B %Y"))
-    newFrame = tkcalendar.formatmonth(app, nextMonth.year, nextMonth.month)
-    if cal_frame is not None:
-        cal_frame.destroy()
-    cal_frame = newFrame
-    newFrame.pack()
+    changemonth(nextMonth)
 
 tkcalendar = TkCalendar(list(tasks.keys()))
 
 header = tkinter.Frame(app)
 header.pack()
 
-tkinter.Button(header, text='<-', command=prevmonth).grid(column=0, row=0, sticky='w')
+tkinter.Button(header, text='<-', command=prevmonth).pack(side=tkinter.LEFT)
 monthLabel = tkinter.Label(header, textvariable=current, font="Roboto 14 bold")
-monthLabel.grid(column = 1, row = 0)
-tkinter.Button(header, text='->', command=nextmonth).grid(column=2, row=0, sticky='e')
+monthLabel.pack(side=tkinter.TOP)
+tkinter.Button(header, text='->', command=nextmonth).pack(side=tkinter.RIGHT)
 
 cal_frame = tkcalendar.formatmonth(app, current_date.year, current_date.month)
 cal_frame.pack()
 
-TaskWindow = tkinter.Tk()
-TaskWindow.title('Tasks')
 
-tasks_frame = tkinter.Frame(TaskWindow)
-tasks_frame.pack()
+tasks_frame = tkinter.Frame(app)
+tasks_frame.pack(side=tkinter.BOTTOM)
 
-tkinter.Label(tasks_frame, text='Today\'s tasks', font='Roboto 12 bold').pack()
+tkinter.Label(tasks_frame, text=f'Today\'s tasks ({datetime.date.today().strftime("%d.%m.")})', font='Roboto 12 bold').pack()
 for date, entries in tasks.items():
     if date.day == datetime.date.today().day:
         for task in entries:
@@ -105,7 +107,6 @@ btn.grid(column = 0, row= 2)
 
 #Launch the GUI
 app.mainloop()
-TaskWindow.mainloop()
 
 #After GUI closes write the tasks back to file (overwriting it)
 #Probably a bit less resource intensive than writing to file every time a change happens
